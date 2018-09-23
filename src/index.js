4e54fa0ac89e4fc90d552f4c0d6240114b5e445e
virus <virus09@mail.ru>
2018-09-23 10:42:51 +0300

    task smart sorter

--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
 class Sorter {
   constructor() {
     // your implementation
+      this.array = [];
+      this.compFunc = (one, two) => one > two;
   }
 
   add(element) {
-    // your implementation
+      // your implementation
+      this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+      // your implementation
+      return this.array[index];
   }
 
   get length() {
-    // your implementation
+      // your implementation
+      return this.array.length;
   }
 
   toArray() {
-    // your implementation
+      // your implementation
+      return this.array;
   }
 
   sort(indices) {
-    // your implementation
+      // your implementation
+      indices.sort();
+      let tmpArr = [];
+      for (let i = 0; i < indices.length; i++) {
+          tmpArr.push(this.array[indices[i]]);
+      }
+
+      tmpArr.sort(this.compFunc);
+
+      for (let i = 0; i < indices.length; i++) {
+          this.array[indices[i]] = tmpArr[i];
+      }
   }
 
   setComparator(compareFunction) {
     // your implementation
+      this.compFunc = compareFunction;
   }
 }
 
